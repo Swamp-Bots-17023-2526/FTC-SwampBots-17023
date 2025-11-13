2d55fd3bc0bf7cbee2e4b133f136e522606be344
DailyDoseOfStuff <allenliu@outlook.com>
2025-11-13 14:38:13 -0600

    Added auto from previous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Autos/BlueBottomAuto.java
@@ -0,0 +1,125 @@
+package org.firstinspires.ftc.teamcode.pedroPathing.Autos;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Subsystems.Flywheel;
+import org.firstinspires.ftc.teamcode.Subsystems.Intake;
+import org.firstinspires.ftc.teamcode.Subsystems.Lift;
+import org.firstinspires.ftc.teamcode.Subsystems.Loader;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+import dev.nextftc.core.components.BindingsComponent;
+import dev.nextftc.core.components.SubsystemComponent;
+import dev.nextftc.extensions.pedro.PedroComponent;
+import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.ftc.components.BulkReadComponent;
+
+@Autonomous(name = "BlueBottom", group = "OpModes")
+public class BlueBottomAuto extends NextFTCOpMode {
+
+    public BlueBottomAuto() {
+        addComponents(new SubsystemComponent(Intake.INSTANCE, Flywheel.INSTANCE, Loader.INSTANCE, Lift.INSTANCE),
+                BulkReadComponent.INSTANCE, BindingsComponent.INSTANCE,
+                new PedroComponent(Constants::createFollower));
+    }
+
+    private Follower follower;
+    private Timer pathTimer, actionTimer, opmodeTimer;
+    private Intake intake;
+    private Flywheel flywheel;
+    private Lift lift;
+    private int pathState;
+
+    private final Pose startRedClose = new Pose(118, 118, Math.toRadians(225));//right up against goal, facing away
+    private final Pose startRedFar = new Pose(84, 12, Math.toRadians(90)); //on line, facing forward, middle of square
+    private final Pose shootPoseFarRed = new Pose(84, 84, Math.toRadians(45)); //CHANGE ONCE VELOCITY DEFINED
+    private final Pose shootPoseCloseRed = new Pose(84, 84, Math.toRadians(45)); //CHANGE ONCE VELOCITY DEFINED
+
+    private final Pose startBlueClose = mirrorPose(startRedClose);
+    private final Pose startBlueFar = mirrorPose(startRedFar);
+    private final Pose shootPoseFarBlue = mirrorPose(shootPoseFarRed);
+    private final Pose shootPoseCloseBlue = mirrorPose(shootPoseCloseRed);
+
+    private final Pose RedObjects1 = new Pose(96, 84, Math.toRadians(180));
+    private final Pose RedEndpickup1 = new Pose(115, 84, Math.toRadians(180));
+    private final Pose RedObjects2 = new Pose(96, 60, Math.toRadians(180));
+    private final Pose RedEndpickup2 = new Pose(115, 60, Math.toRadians(180));
+    private final Pose RedObjects3 = new Pose(96, 36, Math.toRadians(180));
+    private final Pose RedEndpickup3 = new Pose(115, 36, Math.toRadians(180));
+
+    private final Pose BlueObjects1 = mirrorPose(RedObjects1);
+    private final Pose BlueEndpickup1 = mirrorPose(RedEndpickup1);
+    private final Pose BlueObjects2 = mirrorPose(RedObjects2);
+    private final Pose BlueEndpickup2 = mirrorPose(RedEndpickup2);
+    private final Pose BlueObjects3 = mirrorPose(RedObjects3);
+    private final Pose BlueEndpickup3 = mirrorPose(RedEndpickup3);
+
+    private PathChain moveToShoot, moveToObjects1, pickupObjects1, moveToShoot1, moveToObjects2, pickupObjects2, moveToShoot2, moveToObjects3, pickupObjects3, moveToShoot3;
+    public void buildPaths() {
+
+        moveToShoot = follower.pathBuilder()
+                .addPath(new BezierLine(startRedFar, shootPoseFarRed))
+                .setLinearHeadingInterpolation(startRedFar.getHeading(), shootPoseFarRed.getHeading())
+                .build();
+
+        moveToObjects1 = follower.pathBuilder()
+                .addPath(new BezierLine(shootPoseFarRed, BlueObjects1))
+                .setLinearHeadingInterpolation(shootPoseFarRed.getHeading(), BlueObjects1.getHeading())
+                .build();
+
+        pickupObjects1 = follower.pathBuilder()
+                .addPath(new BezierLine(BlueObjects1, BlueEndpickup1))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+
+        moveToShoot1 = follower.pathBuilder()
+                .addPath(new BezierLine(BlueEndpickup1, shootPoseFarRed))
+                .setLinearHeadingInterpolation(BlueEndpickup1.getHeading(), shootPoseFarRed.getHeading())
+                .build();
+
+        moveToObjects2 = follower.
+                pathBuilder().addPath(new BezierLine(shootPoseFarRed, BlueObjects2))
+                .setLinearHeadingInterpolation(shootPoseFarRed.getHeading(), BlueObjects2.getHeading())
+                .build();
+
+        pickupObjects2 = follower.pathBuilder()
+                .addPath(new BezierLine(BlueObjects2, BlueEndpickup2))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+
+        moveToShoot2 = follower.pathBuilder()
+                .addPath(new BezierLine(BlueEndpickup2, shootPoseFarRed))
+                .setLinearHeadingInterpolation(BlueEndpickup2.getHeading(), shootPoseFarRed.getHeading())
+                .build();
+
+        moveToObjects3 = follower.pathBuilder()
+                .addPath(new BezierLine(shootPoseFarRed, BlueObjects3))
+                .setLinearHeadingInterpolation(shootPoseFarRed.getHeading(), BlueObjects3.getHeading())
+                .build();
+
+        pickupObjects3 = follower.pathBuilder()
+                .addPath(new BezierLine(BlueObjects3, RedEndpickup3))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+
+        moveToShoot3 = follower.pathBuilder()
+                .addPath(new BezierLine(RedEndpickup3, shootPoseFarRed))
+                .setLinearHeadingInterpolation(RedEndpickup3.getHeading(), shootPoseFarRed.getHeading())
+                .build();
+
+    }
+
+
+    public Pose mirrorPose(Pose input) {
+        double inputX = input.getX();
+        double inputY = input.getY();
+        double inputHeading = input.getHeading();
+
+        return new Pose(inputX - 72, inputY, 180 - inputHeading);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Autos/RedBottomAuto.java
@@ -0,0 +1,257 @@
+package org.firstinspires.ftc.teamcode.pedroPathing.Autos;
+
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.Subsystems.Shooter;
+import org.firstinspires.ftc.teamcode.Subsystems.Intake;
+import org.firstinspires.ftc.teamcode.Subsystems.Lift;
+import org.firstinspires.ftc.teamcode.Subsystems.Loader;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+
+/*
+ *
+ * START OP MODE RIGHT HALF (RED SIDE)
+ * ON BOTTOM
+ * ON TAPE
+ * FACING TOWARDS OPPOSITE WALL
+ *
+ * */
+@Autonomous(name = "TestAuto", group = "Examples")
+public class RedBottomAuto extends OpMode {
+
+    private Follower follower;
+    private Timer pathTimer, actionTimer, opmodeTimer;
+    private Intake intake;
+    private Shooter flywheel;
+    private Lift lift;
+    private int pathState;
+
+    private final Pose startRedClose = new Pose(118, 118, Math.toRadians(225));//right up against goal, facing away
+    private final Pose startRedFar = new Pose(84, 12, Math.toRadians(90)); //on line, facing forward, middle of square
+    private final Pose shootPoseFarRed = new Pose(84, 84, Math.toRadians(45)); //CHANGE ONCE VELOCITY DEFINED
+    private final Pose shootPoseCloseRed = new Pose(84, 84, Math.toRadians(45)); //CHANGE ONCE VELOCITY DEFINED
+
+    //object poses top to bottom
+    private final Pose RedObjects1 = new Pose(96, 84, Math.toRadians(180));
+    private final Pose RedEndpickup1 = new Pose(115, 84, Math.toRadians(180));
+    private final Pose RedObjects2 = new Pose(96, 60, Math.toRadians(180));
+    private final Pose RedEndpickup2 = new Pose(115, 60, Math.toRadians(180));
+    private final Pose RedObjects3 = new Pose(96, 36, Math.toRadians(180));
+    private final Pose RedEndpickup3 = new Pose(115, 36, Math.toRadians(180));
+
+    private PathChain moveToShoot, moveToObjects1, pickupObjects1, moveToShoot1, moveToObjects2, pickupObjects2, moveToShoot2, moveToObjects3, pickupObjects3, moveToShoot3;
+
+
+    public void buildPaths() {
+
+        moveToShoot = follower.pathBuilder()
+                .addPath(new BezierLine(startRedFar, shootPoseFarRed))
+                .setLinearHeadingInterpolation(startRedFar.getHeading(), shootPoseFarRed.getHeading())
+                .build();
+
+        moveToObjects1 = follower.pathBuilder()
+                .addPath(new BezierLine(shootPoseFarRed, RedObjects1))
+                .setLinearHeadingInterpolation(shootPoseFarRed.getHeading(), RedObjects1.getHeading())
+                .build();
+
+        pickupObjects1 = follower.pathBuilder()
+                .addPath(new BezierLine(RedObjects1, RedEndpickup1))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+
+        moveToShoot1 = follower.pathBuilder()
+                .addPath(new BezierLine(RedEndpickup1, shootPoseFarRed))
+                .setLinearHeadingInterpolation(RedEndpickup1.getHeading(), shootPoseFarRed.getHeading())
+                .build();
+
+        moveToObjects2 = follower.
+                pathBuilder().addPath(new BezierLine(shootPoseFarRed, RedObjects2))
+                .setLinearHeadingInterpolation(shootPoseFarRed.getHeading(), RedObjects2.getHeading())
+                .build();
+
+        pickupObjects2 = follower.pathBuilder()
+                .addPath(new BezierLine(RedObjects2, RedEndpickup2))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+
+        moveToShoot2 = follower.pathBuilder()
+                .addPath(new BezierLine(RedEndpickup2, shootPoseFarRed))
+                .setLinearHeadingInterpolation(RedEndpickup2.getHeading(), shootPoseFarRed.getHeading())
+                .build();
+
+        moveToObjects3 = follower.pathBuilder()
+                .addPath(new BezierLine(shootPoseFarRed, RedObjects3))
+                .setLinearHeadingInterpolation(shootPoseFarRed.getHeading(), RedObjects3.getHeading())
+                .build();
+
+        pickupObjects3 = follower.pathBuilder()
+                .addPath(new BezierLine(RedObjects3, RedEndpickup3))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+
+        moveToShoot3 = follower.pathBuilder()
+                .addPath(new BezierLine(RedEndpickup3, shootPoseFarRed))
+                .setLinearHeadingInterpolation(RedEndpickup3.getHeading(), shootPoseFarRed.getHeading())
+                .build();
+
+    }
+    public void autonomousPathUpdate() {
+        switch (pathState) {
+            case 0:
+                //pause
+                //flywheel shoot
+                follower.followPath(moveToShoot, true);
+                //for shooting 2 balls
+
+                setPathState(1);
+                break;
+            case 1:
+                if (!follower.isBusy()) {
+                    follower.followPath(moveToObjects1, true);
+                    //start intake
+                    setPathState(2);
+                }
+                break;
+            case 2:
+                if (!follower.isBusy()) {
+                    follower.followPath(pickupObjects1, true);
+                    //stop intake
+                    setPathState(3);
+                }
+            case 3:
+                if (!follower.isBusy()) {
+                    follower.followPath(moveToShoot1, true);
+                    //pause
+
+                    setPathState(4);
+                }
+                break;
+            case 4:
+                if (!follower.isBusy()) {
+
+                    follower.followPath(moveToObjects2, true);
+                    //start intake
+                    setPathState(5);
+                }
+                break;
+            case 5:
+                if (!follower.isBusy()) {
+                    follower.followPath(pickupObjects2, true);
+                    //stop intake
+                    setPathState(6);
+                }
+                break;
+            case 6:
+                if (!follower.isBusy()) {
+                    //pause
+                    follower.followPath(moveToShoot2, true);
+
+                    setPathState(7);
+                }
+                break;
+            case 7:
+                if (!follower.isBusy()) {
+                    follower.followPath(moveToObjects3, true);
+                    //start intake
+                    setPathState(8);
+                }
+                break;
+            case 8:
+                if (!follower.isBusy()) {
+                    follower.followPath(pickupObjects3, true);
+                    //stop intake
+                    setPathState(9);
+                }
+                break;
+            case 9:
+                if (!follower.isBusy()) {
+                    //pause
+                    follower.followPath(moveToShoot3, true);
+
+                    setPathState(10);
+                }
+                break;
+        }
+    }
+
+    public Pose mirrorPose(Pose input) {
+        double inputX = input.getX();
+        double inputY = input.getY();
+        double inputHeading = input.getHeading();
+
+        return new Pose(inputX - 72, inputY, 180 - inputHeading);
+    }
+
+    public void setPathState(int set) {
+        pathState = set;
+        pathTimer.resetTimer();
+    }
+
+    /**
+     * This is the main loop of the OpMode, it will run repeatedly after clicking "Play".
+     **/
+    @Override
+    public void onUpdate() {
+
+        // These loop the movements of the robot, these must be called continuously in order to work
+        follower.update();
+        autonomousPathUpdate();
+
+        // Feedback to Driver Hub for debugging
+        telemetry.addData("path state", pathState);
+        telemetry.addData("x", follower.getPose().getX());
+        telemetry.addData("y", follower.getPose().getY());
+        telemetry.addData("heading", follower.getPose().getHeading());
+        telemetry.update();
+    }
+
+    /**
+     * This method is called once at the init of the OpMode.
+     **/
+    @Override
+    public void onInit() {
+        pathTimer = new Timer();
+        opmodeTimer = new Timer();
+        opmodeTimer.resetTimer();
+
+
+        follower = Constants.createFollower(hardwareMap);
+        buildPaths();
+        follower.setStartingPose(startRedFar);
+
+    }
+
+    /** This method is called continuously after Init while waiting for "play". **/
+  /*  @Override
+    public void periodic() {
+
+    }
+*/
+
+    /**
+     * This method is called once at the start of the OpMode.
+     * It runs all the setup actions, including building paths and starting the path system
+     **/
+    @Override
+    public void onStartButtonPressed() {
+        opmodeTimer.resetTimer();
+        setPathState(0);
+    }
+
+    /**
+     * We do not use this because everything should automatically disable
+     **/
+    @Override
+    public void onStop() {
+    }
+
+
+}
